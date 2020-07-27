b2292f43680ca28714e111c45ea944e4595b1c00
md2eoseo <md2eoseo@gmail.com>
2020-07-27 19:38:06 +0900

    sendMail function with nodemailer, mailgun

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,39 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import mgTransport from "nodemailer-mailgun-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+export const sendMail = (email) => {
+  const auth = {
+    auth: {
+      api_key: process.env.MAILGUN_API_KEY,
+      domain: process.env.MAILGUN_DOMAIN,
+    },
+  };
+  const client = nodemailer.createTransport(mgTransport(auth));
+  return client.sendMail(email, (err, info) => {
+    if (err) {
+      console.log(`Error: ${err}`);
+    } else {
+      console.log(`Response: ${info}`);
+    }
+  });
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "md2eoseo@prismagram.com",
+    to: address,
+    subject: "🔒Login Secret for Prismagram🔒",
+    html: `Hello! Your login secret is ${secret}.<br/>Copy paste on the app/wepsite to login.yarn `,
+  };
+  return sendMail(email);
 };
