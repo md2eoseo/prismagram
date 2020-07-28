3777efc6976a9f48fb2b298ef1faf1a79bc8beb5
md2eoseo <md2eoseo@gmail.com>
2020-07-28 15:01:23 +0900

    added passport JWT

--- src/server.js
+++ src/server.js
@@ -1,27 +1,22 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
+import "./env";
 
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import "./passport";
+import { authenticateJwt } from "./passport";
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-type Query{
-    hello: String!
-}`;
-
-const resolvers = {
-  Query: { hello: () => "Hi" },
-};
-
 const server = new GraphQLServer({
   schema,
+  context: ({ request }) => ({
+    request,
+  }),
 });
 
 server.express.use(logger("dev"));
+server.express.use(authenticateJwt);
 
 server.start({ port: PORT }, () =>
   console.log(`✅ Server running on http://localhost:${PORT}`)
