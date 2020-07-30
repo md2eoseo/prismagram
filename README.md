38f4a6a17aec2801be54fa447415ceb700dbf8ce
md2eoseo <md2eoseo@gmail.com>
2020-07-30 09:30:13 +0900

    added searchPost

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ Instagram clone with Express, Prisma, React, and React Native
 - [x] Log in - Confirm secret
 - [x] Like / Unlike a photo
 - [x] Comment on a photo
-- [ ] Search by user
-- [ ] Search by location
+- [x] Search by user
+- [x] Search by location, caption
 - [ ] See user profile
 - [ ] Follow / Unfollow User
 - [ ] See the full photo
---
+++ src/api/Post/searchPost/searchPost.js
@@ -0,0 +1,15 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    searchPost: async (_, args) => {
+      const { term } = args;
+      const posts = await prisma.posts({
+        where: {
+          OR: [{ location_starts_with: term }, { caption_starts_with: term }],
+        },
+      });
+      return posts;
+    },
+  },
+};
