3527f632c3fe9347bf7cf3fe8ebce82f3d6f30ed
md2eoseo <md2eoseo@gmail.com>
2020-08-04 12:27:54 +0900

    added editUser

---
+++ src/api/User/editUser/editUser.js
@@ -0,0 +1,23 @@
+import { isAuthenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    editUser: async (_, args, { request }) => {
+      isAuthenticated(request);
+      const { username, email, firstName, lastName, bio } = args;
+      const { user } = request;
+      // await 써서 const user에 저장하고 return user를 해도되지만 어차피 마지막 요청이라 알아서 서버에서 기다려줌.
+      return prisma.updateUser({
+        where: { id: user.id },
+        data: {
+          username,
+          email,
+          firstName,
+          lastName,
+          bio,
+        },
+      });
+    },
+  },
+};
