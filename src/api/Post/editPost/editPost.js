789af504862a794cf6fb44ae47e9b9f9be81505a
md2eoseo <md2eoseo@gmail.com>
2020-08-07 18:45:25 +0900

    added deleting post action with editPost

--- src/api/Post/editPost/editPost.js
+++ src/api/Post/editPost/editPost.js
@@ -1,20 +1,27 @@
 import { prisma } from "../../../../generated/prisma-client";
 
+const EDIT = "EDIT";
+const DELETE = "DELETE";
+
 export default {
   Mutation: {
     editPost: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      const { postId, caption, location } = args;
+      const { postId, caption, location, action } = args;
       const post = await prisma.$exists.post({
         id: postId,
         user: { id: user.id },
       });
       if (post) {
-        return prisma.updatePost({
-          where: { id: postId },
-          data: { caption, location },
-        });
+        if (action === EDIT) {
+          return prisma.updatePost({
+            where: { id: postId },
+            data: { caption, location },
+          });
+        } else if (action === DELETE) {
+          return prisma.deletePost({ id: postId });
+        }
       } else {
         throw Error("You can't edit the post!");
       }
