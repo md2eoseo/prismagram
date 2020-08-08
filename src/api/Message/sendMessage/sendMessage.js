729373332ef24a1d553cfe6bf855714ee9c8e411
md2eoseo <md2eoseo@gmail.com>
2020-08-08 10:34:15 +0900

    added sendMessage

---
+++ src/api/Message/sendMessage/sendMessage.js
@@ -0,0 +1,43 @@
+import { prisma } from "../../../../generated/prisma-client";
+import { ROOM_FRAGMENT } from "../../../fragments";
+
+export default {
+  Mutation: {
+    sendMessage: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const { roomId, text, toId } = args;
+      let room;
+      if (roomId === undefined) {
+        if (user.id !== toId) {
+          room = await prisma
+            .createRoom({
+              participants: { connect: [{ id: user.id }, { id: toId }] },
+            })
+            .$fragment(ROOM_FRAGMENT);
+        }
+      } else {
+        room = await prisma.room({ id: roomId }).$fragment(ROOM_FRAGMENT);
+      }
+      if (!room) {
+        throw Error("Room not found!");
+      }
+
+      const getTo = room.participants.filter(
+        (participant) => participant.id != user.id
+      )[0];
+      const message = await prisma.createMessage({
+        text,
+        from: { connect: { id: user.id } },
+        to: { connect: { id: roomId ? getTo.id : toId } },
+        room: {
+          connect: {
+            id: room.id,
+          },
+        },
+      });
+
+      return message;
+    },
+  },
+};
