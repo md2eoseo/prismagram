a52f21eb4e915071e757463879773a2d37f6561f
md2eoseo <md2eoseo@gmail.com>
2020-08-13 13:28:01 +0900

    check username, email if there is already taken in db

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,14 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const existsUsername = await prisma.$exists.user({ username });
+      const existsEmail = await prisma.$exists.user({ email });
+      if (existsUsername) {
+        throw Error("This username is already taken");
+      }
+      if (existsEmail) {
+        throw Error("This email is already taken");
+      }
       await prisma.createUser({
         username,
         email,
