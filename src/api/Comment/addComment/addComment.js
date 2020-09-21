68ebdf3af6cb6516fa87d91aefd42df9e138569f
md2eoseo <md2eoseo@gmail.com>
2020-09-21 12:41:21 +0900

    fixed non-nullable field's null return

--- src/api/Comment/addComment/addComment.js
+++ src/api/Comment/addComment/addComment.js
@@ -2,26 +2,24 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    addComment: async (_, args, { request, isAuthenticated }) => {
+    addComment: (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { text, postId } = args;
       const { user } = request;
-      const comment = {
-        text,
-        user: {
-          connect: {
-            id: user.id,
-          },
-        },
-        post: {
-          connect: {
-            id: postId,
-          },
-        },
-      };
       try {
-        await prisma.createComment(comment);
-        return comment;
+        return prisma.createComment({
+          text,
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+          post: {
+            connect: {
+              id: postId,
+            },
+          },
+        });
       } catch (error) {
         console.log(error);
         return;
